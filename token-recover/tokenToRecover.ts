214c11dae2610e53291c665f624f3f83df2fc387
eanmcc8 <eanmcc86@proton.me>
2026-07-17 02:46:10 +0100

    Update tokenToRecover.ts

--- token-recover/tokenToRecover.ts
+++ token-recover/tokenToRecover.ts
@@ -2,7 +2,7 @@
 
 // IMPORTANT: Add only tokens that you recognize.
 
-export const tokensToRecover = [
+export const tokenToRecover = [
   "0xae7ab96520de3a18e5e111b5eaab095312d7fe84", // example token address
   "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // USDC
   "0xdAC17F958D2ee523a2206206994597C13D831ec7", // USDT
